7506e196507377331759687c01818cb53f876097
Karim El-Melhaoui <karims@outlook.com>
2020-08-11 21:00:44 -0400

    started on organization inventory

--- organizations.py
+++ organizations.py
@@ -8,4 +8,51 @@ def get_organizationmembers():
     return response
 
 
-# outstanding work
+
+def create_table():
+    client = boto3.client('dynamodb')
+    response = client.create_table(
+        AttributeDefinitions=[
+            {
+            'AttributeName' : 'AccountId',
+            'AttributeType' : 'S',
+            'AttributeName' : 'Email',
+            'AttributeType' : 'S',
+            'AttributeName' : 'Name',
+            'AttributeType' : 'S'
+            },
+        ],
+        TableName = 'OrganizationInfo',
+        KeySchema=[
+            {
+                'AttributeName' : 'Name',
+                'KeyType' : 'HASH',
+            },
+        ],
+        BillingMode='PAY_PER_REQUEST'
+    )
+    return response
+
+
+
+# need to fix syntax so it can loop over dict.
+
+#table = create_table()
+#print(table)
+
+def populate_table():
+    dynamodb = boto3.resource('dynamodb')
+    table = dynamodb.Table('OrganizationInfo')
+    orgmembers = get_organizationmembers()
+    print(orgmembers)
+    for orgmember in orgmembers['Accounts']:
+        response = table.put_item(
+            Item = {
+                'Name': orgmember['Name'],
+               # 'Email': orgmember['Email'],
+               # 'AccountId': orgmember['Id'],
+            }
+        )
+        return response
+populate_table()
+
