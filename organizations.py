d6cf6129f0c88a7ff8cb57c8c418606cc8d25582
Karim El-Melhaoui <karims@outlook.com>
2020-08-12 18:24:55 -0400

    finished organization to dynamodb script

--- organizations.py
+++ organizations.py
@@ -1,58 +1,73 @@
 import boto3
 import botocore
+import time
 
-client = boto3.client('organizations')
+# Karim El-Melhaoui
+# Retrieves information from an AWS Organizations Master account.
+# Creates a DynamoDB table and populates the retrieved information into DynamoDB.
 
 def get_organizationmembers():
+    client = boto3.client('organizations')
     response = client.list_accounts()
     return response
 
 
-
 def create_table():
     client = boto3.client('dynamodb')
-    response = client.create_table(
-        AttributeDefinitions=[
-            {
-            'AttributeName' : 'AccountId',
-            'AttributeType' : 'S',
-            'AttributeName' : 'Email',
-            'AttributeType' : 'S',
-            'AttributeName' : 'Name',
-            'AttributeType' : 'S'
-            },
-        ],
-        TableName = 'OrganizationInfo',
-        KeySchema=[
-            {
+    try:
+        response = client.create_table(
+            AttributeDefinitions=[
+                {
+                'AttributeName' : 'AccountId',
+                'AttributeType' : 'S',
+                'AttributeName' : 'Email',
+                'AttributeType' : 'S',
                 'AttributeName' : 'Name',
-                'KeyType' : 'HASH',
-            },
-        ],
-        BillingMode='PAY_PER_REQUEST'
-    )
-    return response
+                'AttributeType' : 'S'
+                },
+            ],
+            TableName = 'OrganizationInfo',
+            KeySchema=[
+                {
+                    'AttributeName' : 'Name',
+                    'KeyType' : 'HASH',
+                },
+            ],
+            BillingMode='PAY_PER_REQUEST'
+        )
+    except botocore.exceptions.ClientError as error:
+        print(error)
+    else:
+        print('Table has been created..')
+        return response
+    
 
-
-
-# need to fix syntax so it can loop over dict.
-
-#table = create_table()
-#print(table)
 
 def populate_table():
+    time.sleep(10) # wait to ensure table has been created.
     dynamodb = boto3.resource('dynamodb')
     table = dynamodb.Table('OrganizationInfo')
     orgmembers = get_organizationmembers()
-    print(orgmembers)
-    for orgmember in orgmembers['Accounts']:
-        response = table.put_item(
-            Item = {
-                'Name': orgmember['Name'],
-               # 'Email': orgmember['Email'],
-               # 'AccountId': orgmember['Id'],
-            }
-        )
-        return response
-populate_table()
+    for i in orgmembers['Accounts']:
+        print(i['Id'])
+        try:
+            table.put_item(
+                Item = {
+                'Name': i['Name'],
+                'Email': i['Email'],
+                'AccountId': i['Id'],
+                'Status' : i['Status'],
+                'JoinedMethod' : i['JoinedMethod'],
+                }
+            )
+        except botocore.exceptions.ClientError as error:
+            print(error)
+        else:
+            continue
+    print('Tables have updated populated with account information. Raw data:')
+    print('\n', orgmembers)
 
+
+if __name__ == "__main__":
+    create_table()
+    populate_table()
